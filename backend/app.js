3e914754495b7ffab0dc2e0fb9c1e6b7579434b9
Simone Willemse <simywillemse@gmail.com>
2023-01-22 16:57:11 +0100

    added stuff

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -20,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
